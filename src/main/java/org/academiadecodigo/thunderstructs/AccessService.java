29b9f2541b985a48441fba71f7345ca6294fc1b5
João Furnas <joaofurnas@gmail.com>
2019-12-12 17:10:05 -0100

    solved getUser problem in AccessService class

--- src/main/java/org/academiadecodigo/thunderstructs/AccessService.java
+++ src/main/java/org/academiadecodigo/thunderstructs/AccessService.java
@@ -1,6 +1,7 @@
 package org.academiadecodigo.thunderstructs;
 
 import org.academiadecodigo.thunderstructs.Dto.LoginDto;
+import org.academiadecodigo.thunderstructs.Dto.RegisterDto;
 import org.academiadecodigo.thunderstructs.models.Battle;
 import org.academiadecodigo.thunderstructs.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,8 +51,8 @@ public class AccessService {
         User newUser = new User();
         newUser.setUsername(user.getUsername());
         newUser.setEmail(user.getEmail());
-        newUser.setPassword(user.getPassword);
-        newUser.setName(user.getName);
+        newUser.setPassword(user.getPassword());
+        newUser.setName(user.getName());
         newUser.setLocation(user.getLocation());
 
         dataSource.getAllUsersList().add(newUser);
@@ -82,6 +83,10 @@ public class AccessService {
     }
 
     public static void main(String[] args) {
+
+        AccessService access = new AccessService();
+
+        System.out.println(access.getUser(1).getUsername());
 
     }
 
