619c992bc350e04e0d8a7cce42e35b77f0a8aa18
João Furnas <joaofurnas@gmail.com>
2019-12-12 14:22:27 -0100

    wworking on database model

--- src/main/java/org/academiadecodigo/thunderstructs/DataBase.java
+++ src/main/java/org/academiadecodigo/thunderstructs/DataBase.java
@@ -1,4 +1,31 @@
 package org.academiadecodigo.thunderstructs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataBase {
+
+    private List usersList;
+    private List battlesList;
+    private List activeUsers;
+
+    public DataBase(){
+        usersList = new ArrayList();
+        battlesList = new ArrayList();
+        activeUsers = new ArrayList();
+
+    }
+
+
+    public List getAllUsersList() {
+        return usersList;
+    }
+
+    public List getBattlesList() {
+        return battlesList;
+    }
+
+    public List getActiveUsers() {
+        return activeUsers;
+    }
 }
